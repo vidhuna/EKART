e981b6b658ba4cf875431cf363d099a7c6706391
vidhuna <vidhuna17032007@gmail.com>
2025-12-19 16:16:20 +0530

    Change method visibility to public in UserRepository

--- src/main/java/com/ecommerce/repository/UserRepository.java
+++ src/main/java/com/ecommerce/repository/UserRepository.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.ecommerce.entity.User;
 public class UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-    Optional<User> findByEmail(String email);
-    boolean existsByUsername(String username);
-    boolean existsByEmail(String email);
+   public Optional<User> findByUsername(String username);
+   public Optional<User> findByEmail(String email);
+   public boolean existsByUsername(String username);
+   public boolean existsByEmail(String email);
 
    @Query("SELECT u FROM User u WHERE u.username = ?1 OR u.email = ?2")
    Optional<User> findByUsernameOrEmail(@Param("username") String username, @Param("email") String email);
